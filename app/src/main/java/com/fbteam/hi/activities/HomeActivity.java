26c24c520ebc911928679449d1287868dd4f87ad
nshevchenko <nicola.shevchenko@gmail.com>
2016-03-12 18:52:57 +0000

    mergin

--- app/src/main/java/com/fbteam/hi/activities/HomeActivity.java
+++ app/src/main/java/com/fbteam/hi/activities/HomeActivity.java
@@ -1,30 +1,26 @@
 package com.fbteam.hi.activities;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
-import com.fbteam.hi.App;
 import com.fbteam.hi.R;
 import com.fbteam.hi.adapters.CategoryListAdapter;
-import com.fbteam.hi.ShowQRActivity;
 import com.fbteam.hi.helper.CaptureQRActivityAnyOrientation;
+import com.fbteam.hi.models.App;
 import com.fbteam.hi.models.Category;
 import com.google.zxing.integration.android.IntentIntegrator;
 
-public class HomeActivity extends Activity implements View.OnClickListener  {
+public class HomeActivity extends ActivityNavMenu implements View.OnClickListener  {
 
     // list view
     private ListView categoriesList;
@@ -35,10 +31,12 @@ public class HomeActivity extends Activity implements View.OnClickListener  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-
-
+        setSupportActionBar(toolbar);
+        loadNavDrawer(toolbar);
         findElements();
     }
+
+
 
 
 
@@ -58,7 +56,6 @@ public class HomeActivity extends Activity implements View.OnClickListener  {
 //                clickedShout.createQRCode();
 
 
-
 //                FragmentShoutDetail fragmentShoutDetail = new FragmentShoutDetail();
 //                fragmentShoutDetail.setShoutDetail(clickedShout);
 //
@@ -67,39 +64,6 @@ public class HomeActivity extends Activity implements View.OnClickListener  {
         });
 
     }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
 
     @Override
     public void onClick(View view) {
@@ -114,4 +78,6 @@ public class HomeActivity extends Activity implements View.OnClickListener  {
                 break;
         }
     }
+
+
 }
