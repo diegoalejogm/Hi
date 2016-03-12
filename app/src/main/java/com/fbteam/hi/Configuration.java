d419e0516a11808d3d6ff717c1d239d71f2484c3
nshevchenko <nicola.shevchenko@gmail.com>
2016-03-12 16:18:30 +0000

    configs

--- app/src/main/java/com/fbteam/hi/Configuration.java
+++ app/src/main/java/com/fbteam/hi/Configuration.java
@@ -8,9 +8,16 @@ public class Configuration {
 
     public static final int ONLINE_MODE_APP = 0;
     public static final int OFFLINE_MODE_APP = 1;
+
     public static final int IMPORT_INFO_FROM_FACEBOOK = 0;
     public static final int IMPORT_INFO_FROM_ADDRESSBOOK = 1;
 
     // storage
     public static final String DB_PREFERENCES = "MyPreferences";
+
+
+    public static final int TYPE_LINK_PHONE = 0;
+    public static final int TYPE_LINK_WEBSITE =  1;
+    public static final int TYPE_LINK_NAME =  2;
+
 }
