60c4d2ab40faeccfd62d55c0225d85db725f5bba
Leticia Wanderley <leticiafwanderley@gmail.com>
2016-03-13 06:15:57 +0000

    Website links

--- app/src/main/java/com/fbteam/hi/activities/LoginActivity.java
+++ app/src/main/java/com/fbteam/hi/activities/LoginActivity.java
@@ -109,7 +109,7 @@ public class LoginActivity extends Activity implements View.OnClickListener  {
                                         Link emailLink = new Link("email", email, Configuration.TYPE_LINK_WEBSITE, false);
                                         App.getMe().addLinkNoCategory(emailLink);
                                     }
-                                    Link facebookLink = new Link("facebook", link.replace("https://www.facebook.com/app_scoped_user_id/", ""), Configuration.TYPE_LINK_WEBSITE, true);
+                                    Link facebookLink = new Link("Facebook", link.replace("https://www.facebook.com/app_scoped_user_id/", ""), Configuration.TYPE_LINK_WEBSITE, true);
                                     App.getMe().addLinkNoCategory(facebookLink);
                                     openHomeActivity(Configuration.IMPORT_INFO_FROM_FACEBOOK);
                                 } catch (JSONException e) {
